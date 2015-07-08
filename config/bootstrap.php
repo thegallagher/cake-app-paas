4b2a2f08cba062073f2508c60192cb660bbf74f3
David Gallagher <david@thegallagher.net>
2015-07-08 17:20:10 +1000

    Ignore when DebugKit is missing in debug mode.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -185,7 +185,9 @@ Plugin::load('Migrations');
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
 if (Configure::read('debug')) {
-    Plugin::load('DebugKit', ['bootstrap' => true]);
+    try {
+        Plugin::load('DebugKit', ['bootstrap' => true]);
+    } catch (\Cake\Core\Exception\MissingPluginException $e) { }
 }
 
 /**
